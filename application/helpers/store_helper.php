7e987b91fc0ae9c6d2496ff702cc8205dbfd6a70
Milan Chauhan <milanchauhan431@gmail.com>
2023-05-20 12:39:02 +0530

    gate entry header and table data function

--- application/helpers/store_helper.php
+++ application/helpers/store_helper.php
@@ -10,6 +10,18 @@ function getStoreDtHeader($page){
     $data['storeLocation'][] = ["name"=>"Location"];
     $data['storeLocation'][] = ["name"=>"Remark"];
 
+    /* Gate Entry */
+    $data['gateEntry'][] = ["name" => "Action", "style" => "width:5%;", "textAlign" => "center"];
+    $data['gateEntry'][] = ["name" => "#", "style" => "width:5%;", "textAlign" => "center"];
+    $data['gateEntry'][] = ["name"=> "GE No.", "textAlign" => "center"];
+    $data['gateEntry'][] = ["name" => "GE Date", "textAlign" => "center"];
+    $data['gateEntry'][] = ["name" => "Driver Name"];
+    $data['gateEntry'][] = ["name" => "Driver No."];
+    $data['gateEntry'][] = ["name" => "Vehicle No."];
+    $data['gateEntry'][] = ["name" => "Vehicle Type"];
+    $data['gateEntry'][] = ["name" => "Transport"];
+    $data['gateEntry'][] = ['name' => "No. of Items"];
+
     return tableHeader($data[$page]);
 }
 
@@ -32,5 +44,23 @@ function getStoreLocationData($data){
 	
 	$action = getActionButton($editButton.$deleteButton);
     return [$action,$data->sr_no,$data->store_name,$locationName,$data->remark];
+}
+
+/* Gate Entry Data  */
+function getGateEntryData($data){
+    $deleteParam = "{'postData':{'id' : ".$data->id."},'message' : 'Gate Entry'}";
+    $editParam = "{'postData':{'id' : ".$data->id."},'modal_id' : 'modal-lg', 'form_id' : 'editGateEntry', 'title' : 'Update Gate Entry'}";
+
+    $editButton = "";
+    $deleteButton = "";
+    if($data->trans_status == 0):
+        $editButton = '<a class="btn btn-success btn-edit permission-modify" href="javascript:void(0)" datatip="Edit" flow="down" onclick="edit('.$editParam.');"><i class="ti-pencil-alt" ></i></a>';
+
+        $deleteButton = '<a class="btn btn-danger btn-delete permission-remove" href="javascript:void(0)" onclick="trash('.$deleteParam.');" datatip="Remove" flow="down"><i class="ti-trash"></i></a>';
+    endif;
+
+    $action = getActionButton($editButton.$deleteButton);
+
+    return [$action,$data->sr_no,$data->trans_number,formatDate($data->trans_date),$data->driver_name,$data->driver_contact,$data->vehicle_no,$data->vehicle_type_name,$data->transport_name,$data->no_of_items];
 }
 ?>
