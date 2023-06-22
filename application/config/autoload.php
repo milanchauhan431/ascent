67dadceff535d0c0591f59d81427c02a015dc5fe
Milan Chauhan <milanchauhan431@gmail.com>
2023-06-22 19:28:27 +0530

    add new hepler file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','formatnumber','tabular','configration','human_resource','master','sales','purchase','store');
+$autoload['helper'] = array('url','file','form','formatnumber','tabular','configration','human_resource','master','sales','purchase','store','production');
 
 /*
 | -------------------------------------------------------------------
