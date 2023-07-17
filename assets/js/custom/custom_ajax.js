d067645d066c7b91ce5f69cfe2dcfa0873664070
Milan Chauhan <milanchauhan431@gmail.com>
2023-07-17 17:30:04 +0530

    column search ordering problem fix

--- assets/js/custom/custom_ajax.js
+++ assets/js/custom/custom_ajax.js
@@ -170,6 +170,7 @@ function ssDatatable(ele,tableHeaders,tableOptions,dataSet={}){
 			if($.inArray(index, selectIndex)!= -1) {$(this).html( selectBox[i] );i++;}
 
 			$(this).removeClass("sorting");
+			$(this).addClass("sorting_disabled");
 			$(this).attr("data-orderable",false);
 		});
 	}
