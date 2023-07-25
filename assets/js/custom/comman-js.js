e150427cd8734949be6d9e1e2d01ae67672e6e6b
Milan Chauhan <milanchauhan431@gmail.com>
2023-07-25 19:33:02 +0530

    select2 focus event

--- assets/js/custom/comman-js.js
+++ assets/js/custom/comman-js.js
@@ -362,6 +362,18 @@ $(document).ready(function(){
 		var fileName = $('#'+inputId).val().split('\\').pop() || "Choose file";
         $('label[for="' + inputId + '"]').html(fileName);
 	});
+
+	// on first focus (bubbles up to document), open the menu
+	$(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
+		$(this).closest(".select2-container").siblings('select:enabled').select2('open');
+	});
+	
+	// steal focus during close - only capture once and stop propogation
+	$('select.select2').on('select2:closing', function (e) {
+		$(e.target).data("select2").$selection.one('focus focusin', function (e) {
+			e.stopPropagation();
+		});
+	});
 });
 
 $(window).on('pageshow', function() {
